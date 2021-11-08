d1d2a388dc5333d06ea958182cc98edb37777082
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-08 21:11:22 +0000

    created a function  that returns a string of 6 random alphanumeric characters:

--- express_server.js
+++ express_server.js
@@ -5,17 +5,31 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 
+function generateRandomString() {
+  let random = "";
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++) {
+    random += characters.charAt(Math.floor(Math.random() * characters.length));
+
+  }
+  return random;
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.listen(8080, () => {
-  console.log(`Example app listening on port ${8080}!`);
+  console.log(`Example app listening on port ${PORT}!`);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -36,10 +50,15 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL
+  const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   //shortened //
-  const templateVars = { shortURL, longURL }
+  const templateVars = { shortURL, longURL };
 
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
